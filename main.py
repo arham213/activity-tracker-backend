45a3877d61d91436941fdd80c65557ac2a71b5ca
arham213 <76661263+arham213@users.noreply.github.com>
2026-04-07 00:47:34 +0500

    fix:fixed-allowed-origins

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
 from app.routers import imageToText
+
+load_dotenv()
 
 app = FastAPI(title="Activity Tracker API", version="1.0.0")
 
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "https://activity-tracker-frontend-eta.vercel.app").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # your frontend URL
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
